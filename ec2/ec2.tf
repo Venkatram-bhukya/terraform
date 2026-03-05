23c448dfdfd19a900042ced9d9f81844ad405b85
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-03-05 14:03:20 +0530

    action

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -9,26 +9,24 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "allow_tls" {  # This is for terraform account
   name        = "allow-all-terraform" # this is for AWS account
   description = "Allow TLS inbound traffic and all outbound traffic"
 
-  egress {
+ egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
-  ingress {
+ ingress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   tags = {
     Name = "allow-all-terraform"
   }

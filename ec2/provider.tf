66eaa4dc3fb34c5dd522977de0f8af9652b1bf3a
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-09-10 13:17:02 +0530

    action

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "1.16.2" # Terraform AWS provider version
+      source  = "hashicorp/aws"
+      version = "1.16.2"
     }
   }
 }
